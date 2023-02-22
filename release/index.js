8f513b17d4a08201af7be23136bb88b0f52b2013
vincent evrard <vinceevrardgpv@gmail.com>
2023-02-22 20:17:58 +0100

    update nothing

--- release/index.js
+++ release/index.js
@@ -3,15 +3,13 @@
   midiFighter - index.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2023-02-15 14:05:42
-  @Last Modified time: 2023-02-22 18:52:26
+  @Last Modified time: 2023-02-22 20:17:31
 \*----------------------------------------*/
 "use strict";
 
 var _MidiFighterTwister = _interopRequireDefault(require("./MidiFighterTwister.js"));
 var _tools = require("./common/tools.js");
 var _config = _interopRequireDefault(require("./common/config.js"));
-var _os = _interopRequireDefault(require("os"));
-var _fsExtra = _interopRequireDefault(require("fs-extra"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const {
   PROCESS_NAME: processName,
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@
   midiFighter - index.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2023-02-15 14:05:42
-  @Last Modified time: 2023-02-22 18:52:26
+  @Last Modified time: 2023-02-22 20:17:31
 \*----------------------------------------*/
 import MidiFighterTwister from "./MidiFighterTwister.js";
 import {wait} from "./common/tools.js";
 import conf from "./common/config.js";
-import os from "os";
-import fs from 'fs-extra';
 
 const {
   PROCESS_NAME:processName,
