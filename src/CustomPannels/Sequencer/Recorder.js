24ebcae3e95bb8bb8525d937d156df53b9bb64c3
vincent evrard <vincent@ogre.be>
2024-03-25 19:43:20 +0100

    saveBefore massive upd od Recorder

--- src/CustomPannels/Sequencer/Recorder.js
+++ src/CustomPannels/Sequencer/Recorder.js
@@ -2,81 +2,172 @@
   MFT - Recorder.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2024-03-25 00:46:20
-  @Last Modified time: 2024-03-25 01:06:26
+  @Last Modified time: 2024-03-25 19:41:27
 \*----------------------------------------*/
 
-import {getTime, wait} from "../../common/tools.js";
+import {getTime, wait, capitalize} from "../../common/tools.js";
 
 
 export default class Recorder {
+	static STATES = {
+		stop : 0,
+		record : 1,
+		play : 2,
+		pause : 3
+	};
+
+	set state(value){
+		if(!Object.values(Recorder.STATES).includes(value))throw new Error(`Recorder : try set state to unknown (${value}) accepted : {${Object.keys(Recorder.STATES).map(key=>`${key} : ${Recorder.STATES[key]}`).join(", ")}} `);
+		if(this._state == value)return;
+		const stateName = Object.keys(Recorder.STATES)[value];
+		this._old_state = this._state;
+		this._state = value;
+		this[`on${capitalize(stateName)}`]();
+	}
+
+	get state(){
+		return this._state;
+	}
+
+	get isStopping(){
+		return this._state == Recorder.STATES.stop;
+	}
+	get isRecording(){
+		return this._state == Recorder.STATES.record;
+	}
+	get isPlaying(){
+		return this._state == Recorder.STATES.play;
+	}
+	get isPlausing(){
+		return this._state == Recorder.STATES.pause;
+	}
+
+	get wasStopping(){
+		return this._old_state == Recorder.STATES.stop;
+	}
+	get wasRecording(){
+		return this._old_state == Recorder.STATES.record;
+	}
+	get wasPlaying(){
+		return this._old_state == Recorder.STATES.play;
+	}
+	get wasPlausing(){
+		return this._old_state == Recorder.STATES.pause;
+	}
+
+	async onStop(){
+		console.log("onStop");
+		this.knob.color = [0, 64, 255];
+		// knob.color = [255, 255, 0];
+		await this.askToStop();
+	}
+	onPlay(){
+		console.log("onPlay");
+		this.knob.color = [0, 64, 255];
+
+		this.playBand = this._order.develop(this.masterBand.length>1 ? this.masterBand : this.oldBand);
+		this.oldBand = [...this.masterBand];
+		this.masterBand.length = 0 ;
+		console.log(this.playBand);
+		this.play();
+	}
+	async onPause(){
+		console.log("onPause");
+		await this.askToStop();
+		this.knob.color = [0, 255, 255];
+	}
+	onRecord(){
+		console.log("onRecord");
+		this.knob.color = [255, 0, 0];
+		// this.oldBand = [...this.masterBand];
+		// this.masterBand.length = 0 ;
+		this.record("start", this.knob.value);
+	}
 	constructor(knob, midiOut){
-		this.isRecording = false;
-		this.isPlaying = false;
-		this.band = [];
-
-		// this.oldBand = []; 	 // to recover if master is empty so set play/pause by simple click
-		// this.masterBand = []; // the record is apply on this
-		// this.playBand = []; 	 // this is a copy of the master to play with order functionnality
-
-		this._timeScale = 1;
-		knob.color = [0, 64, 255]
-		knob.onPressed( async () => {
-				this.isRecording = true;
-				knob.color = [255, 0, 0];
-				this.isPlaying && await this.askToStop();
-				this.band.length = 0 ;
-				this.record("start", knob.value);
-			})
-			.onReleased( () => {
-				this.isRecording = false;
-				knob.color = [0, 64, 255];
-				this.record("stop", knob.value)
-				this.play();
-				this.isPlaying = true;
-			})
-			.onDoubleClick(async ()=>{
-				knob.color = [255, 255, 0];
-				this.isPlaying && await this.askToStop();
-				this.band.length = 0;
-				knob.color = [0, 64, 255];
-			})
-			.onTurn( inc => {
-				knob.value = (knob.value + inc + 128) % 128;
-				this.isRecording && this.record("change", knob.value);
-				midiOut(knob.id, knob.value)
-			});
+		
 		this.knob = knob;
 		this.midiOut = midiOut;
+		this._old_state;
+		this.state = Recorder.STATES.stop;
+
 		this.hasToStop = null;
+
+
+		this.band = [];
+		this._order;
+		this.oldBand = []; 	 // to recover if master is empty so set play/pause by simple click
+		this.masterBand = []; // the record is apply on this
+		this.playBand = []; 	 // this is a copy of the master to play with order functionnality
+		this._timeScale = 1;
+		
+
+
+		knob.onPressed( async () => {
+				this.state = Recorder.STATES.record;
+				// this.isRecording = true;
+				// knob.color = [255, 0, 0];
+				// this.isPlaying && await this.askToStop();
+				// this.oldBand = [...this.masterBand];
+				// this.masterBand.length = 0 ;
+				// this.record("start", knob.value);
+		})
+		.onReleased( () => {
+			if(this.masterBand.length == 1 && this.wasPlaying){
+				this.state = Recorder.STATES.pause;
+			}else{
+				this.state = Recorder.STATES.play;	
+			}
+			
+			// this.isRecording = false;
+			// knob.color = [0, 64, 255];
+			// this.record("stop", knob.value);
+
+			// this.playBand = this._order.develop(this.masterBand);
+			// this.play();
+			// this.isPlaying = true;
+		})
+		.onDoubleClick(async ()=>{
+			this.state = Recorder.STATES.stop;
+			// knob.color = [255, 255, 0];
+			// this.isPlaying && await this.askToStop();
+			// this.oldBand = [...this.masterBand];
+			// this.masterBand.length = 0 ;
+			// knob.color = [0, 64, 255];
+		})
+		.onTurn( inc => {
+			knob.value = (knob.value + inc + 128) % 128;
+			this.isRecording && this.record("change", knob.value);
+			midiOut(knob.id, knob.value)
+		});
+		
 	}
 	record(name, value){
-		this.band.push({
+		this.masterBand.push({
 			eventName : name, 
 			time : getTime(),
 			value : value
 		});
 	}
-	stop(){
-		this.knob.strobTag = 0.0;
-		this.isPlaying = false;
-		if(this.hasToStop != null) this.hasToStop();
-		this.hasToStop = null;
-	}
 
 	async play(){
 		this.knob.strobTag = 0.5;
-		const current = this.band[0];
-		const next = this.band[1%this.band.length];
-		if(!current || !next) return this.stop();
-
-		this.knob.value = current.value;
-		this.midiOut(this.knob.id, this.knob.value);
-
-		this.band.push(this.band.shift());
-
-		await wait((next.time - current.time) * this._timeScale);
-		if(this.hasToStop != null) return this.stop();
-		this.play();
+		const current = this.playBand[0];
+		const next = this.playBand[1%this.playBand.length];
+		if(current && next){
+			this.knob.value = current.value;
+			this.midiOut(this.knob.id, this.knob.value);
+			this.playBand.push(this.playBand.shift());
+			await wait((next.time - current.time) * this._timeScale);
+		}else if(this.hasToStop == null){
+			this.state = Recorder.STATES.stop;
+		}
+		if(this.hasToStop != null) {
+			this.knob.strobTag = 0.0;
+			this.hasToStop();
+			this.hasToStop = null;
+		}else{
+			this.play();	
+		}
 	}
 	async askToStop(){
 		await new Promise((resolve, reject)=>{
@@ -84,7 +175,10 @@ export default class Recorder {
 		});
 	}
 	set order(value){
-		// copy the master band to have play band 
+		this._order = value;
+	}
+	get order(){
+		return this._order;
 	}
 	set speed(value){
 		let tmp = 128-value;
@@ -98,9 +192,3 @@ export default class Recorder {
 	}
 }
 
-Recorder.ORDER = {
-	NORMAL : 0,
-	REVERSE : 1,
-	PINGPONG : 2,
-	RANDOM : 3 
-}
--- src/CustomPannels/Sequencer/index.js
+++ src/CustomPannels/Sequencer/index.js
@@ -2,7 +2,7 @@
   MFT - index.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2024-03-25 00:45:52
-  @Last Modified time: 2024-03-25 01:02:53
+  @Last Modified time: 2024-03-25 18:03:26
 \*----------------------------------------*/
 
 import Pannel from '../../Bases/Pannel.js';
@@ -13,15 +13,23 @@ import Controler from './Controler.js';
 export default class SequencerPannel extends Pannel{
 	constructor(id, midiDisplay, midiOut){
 		super(id, midiDisplay);
+
+		
 		this.recorders = this.knobs
 			.filter((knob, id)=>id % 4 == 0 || id % 4 == 3)
 			.map( knob => new Recorder(knob, midiOut))
-		this.speeds = this.knobs
+		this.controler = this.knobs
 			.filter((knob, id)=>id % 4 == 1 || id % 4 == 2)
 			.map( knob => new Controler(knob, midiOut))
 			.map( (controler, id) => {
 				controler.onSpeeder( value => this.recorders[id].speed = value);
 				controler.onOrderer( value => this.recorders[id].order = value);
+				return controler;
 			});
+		
+		this.recorders = this.recorders.map( (recorder, id) =>{
+			recorder.order = this.controler[id].orderer.current;
+			return recorder;
+		});
 	}
 }
